12f48ca208f7cde2a281549ff34a4633261e4e35
diego.ravignani <diego.ravignani@gmail.com>
2024-01-02 12:46:04 -0300

    Fixing PEP8 warnings

--- fitpy/likelihood.py
+++ fitpy/likelihood.py
@@ -5,13 +5,11 @@ from scipy.stats import chi2
 
 class LeastSquares:  
 
-
     def __init__(self, x, y, ysigma, model):  
         self.x = x
         self.y = y
         self.ysigma = ysigma
         self.model = model
-            
 
     def cost_function(self, theta): 
         mu = self.model(self.x, theta)        
@@ -19,67 +17,55 @@ class LeastSquares:
         cost = np.sum(residuals**2)
         return cost
 
-
     def __call__(self, seed):
         self.fit_result = minimize(self.cost_function, x0=seed)
 
-        if self.fit_result.success == False:
+        if not self.fit_result.success:
             print(self.fit_result)
             raise FloatingPointError("ERROR: scipy.optimize.minimize did not converge")
 
         return self.fit_result.status
-        
 
     # Fit results getters
-    
     def get_estimators(self):
         return self.fit_result.x
-        
-        
+
     def get_errors(self):
         covariance = self.get_covariance_matrix()
-        errors = np.sqrt( np.diagonal(covariance) )
+        errors = np.sqrt(np.diagonal(covariance))
         return errors
-        
-        
+
     def get_covariance_matrix(self):
         covariance = 2 * self.fit_result.hess_inv
         return covariance
-        
-    
+
     def get_correlation_matrix(self):
         covariance = self.get_covariance_matrix()
         errors = self.get_errors()
         correlation = covariance / np.tensordot(errors, errors, axes=0)
         return correlation
-    
-    
+
     def get_deviance(self):
         return self.fit_result.fun
-        
-        
+
     def get_ndof(self):
         estimators = self.get_estimators()
         ndof = len(self.x) - len(estimators)
         return ndof
-        
-        
+
     def get_pvalue(self):
         deviance = self.get_deviance()
         ndof = self.get_ndof()
         pvalue = chi2.sf(deviance, ndof)
         return pvalue
-        
-        
+
     def get_minimize_result(self):
         return self.fit_result
-        
-        
+
     def get_yfit(self, x):
         estimators = self.get_estimators()
         return self.model(x, estimators) 
-        
-        
+
     def get_yfit_error(self, x):
 
         gradient = self.get_gradient(x)
@@ -90,7 +76,6 @@ class LeastSquares:
         sigma_yfit = np.sqrt(var_yfit)
         
         return sigma_yfit
-
 
     # Numerical derivative of the model wrt the parameters evaluated at the estimators
     """
@@ -108,7 +93,7 @@ class LeastSquares:
 
         # Setting finite difference steps to some fraction of the errors
         delta_fraction = 0.01
-        steps = errors * 0.01
+        steps = errors * delta_fraction
         ndimensions = len(steps)
 
         gradient = []
@@ -137,9 +122,3 @@ class LeastSquares:
 #        mu = self.fit_model(theta)
 #        cost_array = 2 * (mu - self.k) - 2 * self.k * np.log(mu / self.k)
 #        return cost_array.sum()
-
-
-
-
-    
-
