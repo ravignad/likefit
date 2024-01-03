d9867404ddae76d0397bd19aa1a7c27dd31b49e6
diego.ravignani <diego.ravignani@gmail.com>
2024-01-03 15:21:07 -0300

    Start removing pandas dependencies

--- examples/least_squares/least_squares.py
+++ examples/least_squares/least_squares.py
@@ -1,7 +1,6 @@
 # Example of fitting data with the least squares method
 
 import numpy as np
-import pandas as pd
 
 from likelihood import LeastSquares
 
@@ -11,10 +10,11 @@ def fit_model(x, par):
     return par[0] * np.exp(par[1] * x)
 
 
-input_file = "least_squares.csv"
-data = pd.read_csv(input_file)
+xdata = np.array([0., 0.2, 0.4, 0.6, 0.8, 1., 1.2, 1.4, 1.6, 1.8, 2.])
+ydata = np.array([0.92, 0.884, 0.626, 0.504, 0.481, 0.417, 0.288, 0.302, 0.177, 0.13, 0.158])
+ysigma = np.array([0.1, 0.082, 0.067, 0.055, 0.045, 0.037, 0.03, 0.025, 0.02, 0.017, 0.014])
 
-fit = LeastSquares(data["x"], data["y"], data["dy"], fit_model)
+fit = LeastSquares(xdata, ydata, ysigma, fit_model)
 seed = np.array([0, 0])
 fit(seed)
 
