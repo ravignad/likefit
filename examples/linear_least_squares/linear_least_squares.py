004e008a72020d96e0ba1bfebc5e5edb1f556669
diego.ravignani <diego.ravignani@gmail.com>
2024-01-09 11:53:12 -0300

    Simplification of the linear least squares example

--- examples/linear_least_squares/linear_least_squares.py
+++ examples/linear_least_squares/linear_least_squares.py
@@ -3,7 +3,7 @@
 import numpy as np
 import likefit
 
-xdata = np.array([1.02, 1.06, 1.1, 1.14, 1.18, 1.22, 1.26, 1.3, 1.34])
+xdata = np.array([-0.18, -0.14, -0.1, -0.06, -0.02,  0.02,  0.06,  0.1,  0.14])
 ydata = np.array([2.243, 2.217, 2.201, 2.175, 2.132, 2.116, 2.083, 2.016, 2.004])
 ysigma = np.array([0.008, 0.008, 0.01, 0.009, 0.011, 0.016, 0.018, 0.021, 0.017])
 npar = 2
@@ -11,7 +11,7 @@ npar = 2
 
 # Model linear in the parameters
 def fit_model(x, par):
-    return par[0] + par[1] * (x-1.2)
+    return par[0] + par[1] * x
 
 
 # Create the fitter
